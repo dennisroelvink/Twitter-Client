fe4442b89dfecdb981f2dcf4e576619e913c3ef3
Sharon237 <sf.engbers@gmail.com>
2015-05-10 14:23:35 +0200

    test

--- Twitter-Client/src/nl/saxion/twitter_client/MainActivity.java
+++ Twitter-Client/src/nl/saxion/twitter_client/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		// hallo
 		TweetApplication app = (TweetApplication) getApplicationContext();
 		model = app.getModel();
 		
